1c1a4f8d6d6fd90379b37c8e3238f2b2686a832b
ecsd-gituser <ecsd.github@eagleschools.net>
2024-10-18 08:38:52 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "ECSD Kiosk APP",
+   "homepage": "https://ecsd-gituser.github.io/Kiosk-Webpage/",
+   "enableNavBttns": true,
+   "enableHomeBttn": true,
+   "enableReloadBttn": true,
+   "sessionDataTimeoutTime": 2,
+   "sessionTimeoutTime": 3,
+   "kioskEnabled": true
+};
